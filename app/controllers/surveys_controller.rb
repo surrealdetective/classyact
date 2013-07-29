e668876a58be5a8a36157fd6d95b614237f173bf
surrealdetective <thefifth@gmail.com>
2013-07-29 19:01:41 -0400

    fixes controller for survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,14 @@
 class SurveysController < ApplicationController
+
+  def index
+    
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Classyact::Application.routes.draw do
   resources :surveys
 
-
+  root :to => "surveys#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
