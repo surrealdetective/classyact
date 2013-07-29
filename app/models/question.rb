09162df3d12288793d34ce90da1489d883a87f29
surrealdetective <thefifth@gmail.com>
2013-07-29 19:27:23 -0400

    associates question and choice models

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,8 @@ class Question < ActiveRecord::Base
   attr_accessible :content, :survey_id
 
   belongs_to :survey
+  has_many :choices
 
   def build_with_agree_choices(q_content)
-    self.
   end
 end
