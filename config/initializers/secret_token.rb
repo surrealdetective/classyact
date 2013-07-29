09f6e3d44695ed45b9fc10dc89c7c720e41c08f3
surrealdetective <thefifth@gmail.com>
2013-07-29 18:50:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classyact::Application.config.secret_token = '8ab8c23f379e68e65ff446309bf3f9c5d7d98e9d4a9b682b6b4db328f3e813d3469d85f13d071e37e3fe46d7b256198a9ed53c8ce8fd991d40ba003090217b92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Classyact::Application.config.session_store :cookie_store, key: '_classyact_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Classyact::Application.config.session_store :active_record_store
