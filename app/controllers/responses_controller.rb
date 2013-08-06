c9b0c96d0e1bf76209252ce30e80e1cea82cbacf
surrealdetective <thefifth@gmail.com>
2013-08-06 19:14:37 -0400

    destroys snapshot model;

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -12,6 +12,9 @@ class ResponsesController < ApplicationController
     raise params.inspect
     @student = Student.find_by_id(params[:student_id])
     #use the choices[] syntax?
+    params[:questions].each do |question|
+      @student.responses.build(:choice_id => params[question])
+    end
     @student.add_responses(params[:responses])
   end
 end
