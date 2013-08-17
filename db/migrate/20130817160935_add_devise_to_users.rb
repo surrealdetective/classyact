683220a4744e95b0eaca9fa51220ddc0a2da4dc4
surrealdetective <thefifth@gmail.com>
2013-08-17 12:21:59 -0400

    runs rake db:migrate w/ devise w/ rake db:drop

--- db/migrate/20130817160935_add_devise_to_users.rb
+++ db/migrate/20130817160935_add_devise_to_users.rb
@@ -1,5 +1,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
+    remove_column :users, :email
     change_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
@@ -46,6 +47,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
   end
 
   def self.down
+    add_column :users, :email, :string
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
     raise ActiveRecord::IrreversibleMigration
