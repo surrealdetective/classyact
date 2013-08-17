5958c9bb086ed524d4a27fc9c316cc005c78e4d0
surrealdetective <thefifth@gmail.com>
2013-08-17 12:09:02 -0400

    adds devise and setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Classyact::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Adds devise (add something for production)
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
