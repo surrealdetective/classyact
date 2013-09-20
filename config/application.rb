7bf3efc4e390da423f7ea5a00024079b55371dd1
surrealdetective <thefifth@gmail.com>
2013-09-20 09:41:26 -0400

    adds js files not included in application.js to circumvent 500 production error

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Classyact
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Add Individual Stylesheets and JS files
+    config.assets.precompile += ['import.js', 'improve.js', 'perform.js']
   end
 end
