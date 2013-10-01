c605a68def51e3e950840d52faeac9ed852af819
surrealdetective <thefifth@gmail.com>
2013-10-01 17:59:48 -0400

    creates listable module to further clean look of Survey
    attempting to use constantize for to_const but an error occurs for unknown reasons. possibly because the method is in a module the scope is wrong, or a quirky thing about constantnize, might try Object.get_const

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -11,22 +11,11 @@ class Survey < ActiveRecord::Base
   validates :size, :presence => true
 
   require 'modules/questionable.rb'
+  require 'modules/listable.rb'
   include Questionable
+  include Listable
 
-  #Current Ranges for Questions by Index
-  THINKING = 0..7
-  META_THINKING = 8..10
-  EXPECTATIONS = 11..18
-  META_EXPECTATIONS = 19..21
-  INTERACTIONS = 22..29
-  META_INTERACTIONS = 30..32
-  DISCIPLINE = 33..40
-  META_DISCIPLINE = 41..43
-  WILLING = 44..51
-  META_WILLING = 52..54
-  DIRECTION = 55..62
-  META_DIRECTION = 63..65
-  META_META = 66..68
+
   # {:meta_import => 66, :meta_improve => 67, :meta_perform => 68}
 
   def method_missing(m, *args)
@@ -64,12 +53,12 @@ class Survey < ActiveRecord::Base
   #Calculates the a student's avg scores given an array of all their responses
   def student_factor_scores(student_response)
     scores = {}
-    scores[:thinking] = student_response[THINKING].inject{|sum, element| sum + element} / 8.0
+    scores[:thinking]     = student_response[THINKING].inject{|sum, element| sum + element} / 8.0
     scores[:expectations] = student_response[EXPECTATIONS].inject{|sum, element| sum + element} / 8.0
     scores[:interactions] = student_response[INTERACTIONS].inject{|sum, element| sum + element} / 8.0
-    scores[:discipline] = student_response[DISCIPLINE].inject{|sum, element| sum + element} / 8.0
-    scores[:willing] = student_response[WILLING].inject{|sum, element| sum + element} / 8.0
-    scores[:direction] = student_response[DIRECTION].inject{|sum, element| sum + element} / 8.0
+    scores[:discipline]   = student_response[DISCIPLINE].inject{|sum, element| sum + element} / 8.0
+    scores[:willing]      = student_response[WILLING].inject{|sum, element| sum + element} / 8.0
+    scores[:direction]    = student_response[DIRECTION].inject{|sum, element| sum + element} / 8.0
     return scores
   end
 
@@ -77,48 +66,6 @@ class Survey < ActiveRecord::Base
   def find_student_factor_scores(student)
     @score_totals = self.student_score(student)
     self.student_factor_scores(@score_totals)
-  end
-
-  def to_const(factor)
-    case factor
-    when :thinking
-      THINKING
-    when :expectations
-      EXPECTATIONS
-    when :interactions
-      INTERACTIONS
-    when :discipline
-      DISCIPLINE
-    when :willing
-      WILLING
-    when :direction
-      DIRECTION
-    end
-  end
-
-  def sub_factors(factor)
-    case factor
-    when :thinking
-      ['Necessary Skills', 'Talent Leverage', 'Life Decisions', 'Value-add Skills']
-    when :expectations
-      ['Observing', 'Challeng-ing', 'Comprehensiveness', 'Durability']
-    when :interactions
-      ['Outside-inside', 'Inside-inside', 'Inside-outside', 'Outside-outside']
-    when :discipline
-      ['Planned', 'Scaffold-ed', 'Focused', 'Scoped']
-    when :willing
-      ['Timely', 'Substan-tive', 'Possibility to Act', 'Action']
-    when :direction
-      ['Comfort', 'Curiosity', 'Activity', 'Order']
-    end
-  end
-
-  def factors
-    ["thinking", "interactions", "willing", "discipline", "direction", "expectations"]
-  end
-
-  def question_categories
-    [:thinking, :interactions, :willing, :discipline, :direction, :expectations, :overview]
   end
 
   #Finds sub factor scores per factor
---
+++ lib/modules/listable.rb
@@ -0,0 +1,61 @@
+module Listable
+  #Current Ranges for Questions by Index
+  THINKING = 0..7
+  META_THINKING = 8..10
+  EXPECTATIONS = 11..18
+  META_EXPECTATIONS = 19..21
+  INTERACTIONS = 22..29
+  META_INTERACTIONS = 30..32
+  DISCIPLINE = 33..40
+  META_DISCIPLINE = 41..43
+  WILLING = 44..51
+  META_WILLING = 52..54
+  DIRECTION = 55..62
+  META_DIRECTION = 63..65
+  META_META = 66..68
+
+  #To Access Ranges
+  def to_const(factor)
+    # factor.upcase.constantize
+    case factor
+    when :thinking
+      THINKING
+    when :expectations
+      EXPECTATIONS
+    when :interactions
+      INTERACTIONS
+    when :discipline
+      DISCIPLINE
+    when :willing
+      WILLING
+    when :direction
+      DIRECTION
+    end
+  end
+
+  def sub_factors(factor)
+    case factor
+    when :thinking
+      ['Necessary Skills', 'Talent Leverage', 'Life Decisions', 'Value-add Skills']
+    when :expectations
+      ['Observing', 'Challeng-ing', 'Comprehensiveness', 'Durability']
+    when :interactions
+      ['Outside-inside', 'Inside-inside', 'Inside-outside', 'Outside-outside']
+    when :discipline
+      ['Planned', 'Scaffold-ed', 'Focused', 'Scoped']
+    when :willing
+      ['Timely', 'Substan-tive', 'Possibility to Act', 'Action']
+    when :direction
+      ['Comfort', 'Curiosity', 'Activity', 'Order']
+    end
+  end
+
+  def factors
+    ["thinking", "interactions", "willing", "discipline", "direction", "expectations"]
+  end
+
+  def question_categories
+    [:thinking, :interactions, :willing, :discipline, :direction, :expectations, :overview]
+  end
+
+end
